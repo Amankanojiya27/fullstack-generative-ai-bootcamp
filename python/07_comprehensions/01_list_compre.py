88017cc89e1a561db761665a58db205685416e62
AMAN <amankanojiya815@gmail.com>
2025-11-19 18:16:02 +0530

    Learn list comprehension with examples

---
+++ python/07_comprehensions/01_list_compre.py
@@ -0,0 +1,16 @@
+menu = [
+    "Masala Chai",
+    "Iced Lemon Tea",
+    "Green Tea",
+    "Iced Peach Tea",
+    "Ginger Tea"
+]
+# -------------------------------------
+# Syntax - [expression for item in iterable if condition]
+#             |             |         |       |  |
+#             V             V         V       V  V
+iced_tea = [ my_tea for my_tea in   menu     if "Iced" in my_tea]
+print(iced_tea)
+# -------------------------------------
+len_tea = [tea for tea in menu if len(tea) < 12]
+print(len_tea)
