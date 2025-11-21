f92c9a4f9f489ecb079677db40cd0803bb74a8c4
AMAN <amankanojiya815@gmail.com>
2025-11-21 18:43:47 +0530

    Add syntax comments for dictionary comprehension in tea price conversion

--- python/07_comprehensions/03_dist_compre.py
+++ python/07_comprehensions/03_dist_compre.py
@@ -3,7 +3,11 @@ tea_prices_inr = {
     "Green Chai":30,
     "Lemon Chai":200,
 }
-
+# -------------------------------------
+#      Syntax - [expression for item in iterable if condition]
+#                    |             |         |       |  |
+#                  key:value
+#                    |
 tea_prices_usd = { tea:price/80 for tea, price in tea_prices_inr.items()}
 
 print(tea_prices_usd)
