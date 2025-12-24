d20f5cf1a595e2b0592e68fce6585361c49e6814
AMAN <amankanojiya815@gmail.com>
2025-12-24 16:41:58 +0530

    Learn  Method Resolution Order - MRO

---
+++ python/10_oop/08_mro.py
@@ -0,0 +1,16 @@
+# Method Resolution Order - MRO
+
+class A:
+    lable = "A: Base class"
+    
+class B(A):
+    lable = "B: Masala blend"
+    
+class C(A):
+    lable = "C: Herbal blend"
+    
+class D(B,C):
+    pass
+cup = D()
+
+print(cup.lable)
